59145c2fcca7bb7da4bc8e409d57a0d2210e6ce0
dhille98 <dhillevajja1@gmail.com>
2024-10-22 16:47:47 +0530

    add nsg and rules

---
+++ terraform/day-1/nsg.tf
@@ -0,0 +1,62 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "azb45-rg-nsg"
+  location            = azurerm_resource_group.azb45-rg.location
+  resource_group_name = azurerm_resource_group.azb45-rg.name
+
+
+
+  tags = {
+    environment = "Dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "nsg-rule-1" {
+  name                        = "open-ssh"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.azb45-rg.name
+  network_security_group_name = azurerm_network_security_group.azurerm_network_security_group.name
+}
+resource "azurerm_network_security_rule" "nsg-rule-1" {
+  name                        = "open-http"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.azb45-rg.name
+  network_security_group_name = azurerm_network_security_group.azurerm_network_security_group.name
+}
+resource "azurerm_network_security_rule" "nsg-rule-3" {
+  name                        = "open-sql"
+  priority                    = 130
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3387"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.azb45-rg.name
+  network_security_group_name = azurerm_network_security_group.azurerm_network_security_group.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "sub-aso-1" {
+  subnet_id                 = azurerm_subnet.azb45-rg-subnet-1.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  depends_on = [ azurerm_network_security_group.nsg, azurerm_subnet.azb45-rg-subnet-1 ]
+}
+resource "azurerm_subnet_network_security_group_association" "sub-aso-2" {
+  subnet_id                 = azurerm_subnet.azb45-rg-subnet-2.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  depends_on = [ azurerm_network_security_group.nsg, azurerm_subnet.azb45-rg-subnet-2 ]
+}

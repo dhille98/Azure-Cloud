0db8efcebd97d6e2c12d17a9f5eb761ad4920ee2
dhille98 <dhillevajja1@gmail.com>
2024-10-22 16:30:24 +0530

    add subnetes

--- terraform/day-1/vnet.tf
+++ terraform/day-1/vnet.tf
@@ -10,3 +10,20 @@ resource "azurerm_virtual_network" "azb45-rg-vnet" {
     environment = "Dev"
   }
 }
+
+resource "azurerm_subnet" "azb45-rg-subnet-1" {
+  name                 = "azb45-rg-subnet-1"
+  resource_group_name  = azurerm_resource_group.azb45-rg.name
+  virtual_network_name = azurerm_virtual_network.azb45-rg-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+
+}
+resource "azurerm_subnet" "azb45-rg-subnet-2" {
+  name                 = "azb45-rg-subnet-2"
+  resource_group_name  = azurerm_resource_group.azb45-rg.name
+  virtual_network_name = azurerm_virtual_network.azb45-rg-vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+
+}

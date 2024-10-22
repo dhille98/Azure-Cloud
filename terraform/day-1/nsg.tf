e9beade986e118a3909aa034d80260664b897716
dhille98 <dhillevajja1@gmail.com>
2024-10-22 16:57:13 +0530

    add

--- terraform/day-1/nsg.tf
+++ terraform/day-1/nsg.tf
@@ -12,12 +12,12 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_network_security_rule" "nsg-rule-1" {
   name                        = "open-ssh"
-  priority                    = 210
+  priority                    = 110
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "22"
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.azb45-rg.name
@@ -29,12 +29,12 @@ resource "azurerm_network_security_rule" "nsg-rule-1" {
 }
 resource "azurerm_network_security_rule" "nsg-rule-2" {
   name                        = "open-http"
-  priority                    = 220
+  priority                    = 120
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "80"
+  destination_port_range      = "3387"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.azb45-rg.name
@@ -45,12 +45,12 @@ resource "azurerm_network_security_rule" "nsg-rule-2" {
 }
 resource "azurerm_network_security_rule" "nsg-rule-3" {
   name                        = "open-sql"
-  priority                    = 230
+  priority                    = 130
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "3387"
+  destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.azb45-rg.name

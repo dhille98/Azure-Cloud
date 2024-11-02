1c55ab191f26d4ad9111574a4bd609085daf8d48
dhille98 <dhillevajja1@gmail.com>
2024-11-02 12:19:12 +0530

    add the backend

--- terraform/day-1/0.provider.tf
+++ terraform/day-1/0.provider.tf
@@ -7,6 +7,14 @@ terraform {
   }
 
   required_version = ">= 0.12" # specify your Terraform version requirement
+
+  backend "azurerm" {
+    resource_group_name = "AZB45-HUB-RG"
+    storage_account_name = "terraform45"
+    container_name = "state-file"
+    key = "terraform.tfstate"
+    
+  }
 }
 
 provider "azurerm" {

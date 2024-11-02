ed668e55c2e0c28259b86aeda5ac9b3b6c2346cc
dhille98 <dhillevajja1@gmail.com>
2024-11-02 15:42:55 +0530

    add the terraform import

--- terraform/terraform_import/1.main.tf
+++ terraform/terraform_import/1.main.tf
@@ -9,3 +9,10 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+resource "azurerm_resource_group" "terraform-import" {
+  name     = "terraform-import"
+  location = "East US"  # Make sure the location matches the actual resource
+}
+
+# /subscriptions/b59b4f26-580b-4477-b69c-40a4ac038382/resourceGroups/terraform-import
+# terraform import azurerm_resource_group.example /subscriptions/b59b4f26-580b-4477-b69c-40a4ac038382/resourceGroups/terraform-import
